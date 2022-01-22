3ecd28317c3f8ccdd67ea35b68be9f69c32b4c39
Monique Silva <moniquesntsilva@gmail.com>
2022-01-22 14:58:12 -0300

    feat: add new sections

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
-  <div v-if="loading">Loading...</div>
+  <div
+    v-if="loading"
+    class="w-screen h-screen flex flex-col gap-y-8 items-center justify-center"
+  >
+    <div
+      role="status"
+      class="w-40 h-40 border-t-4 border-b-4 border-gray-900 rounded-full animate-spin"
+    ></div>
+    <span class="visually-hidden text-3xl text-gray-900">Loading...</span>
+  </div>
   <div v-else>
     <Header />
-    <Hero v-bind="info[0]" />
+    <Carousel v-bind="info[0]" />
     <main>
       <section>
         <h2>Featured Movie</h2>
         <div v-for="movie in info" :key="movie.id">
+          <Movies v-bind="movie" />
+        </div>
+      </section>
+      <section>
+        <h2>New Arrival</h2>
+        <div v-for="movie in currentYear" :key="movie.id">
           <Movies v-bind="movie" />
         </div>
       </section>
@@ -17,19 +32,20 @@
 <script>
 import axios from 'axios'
 import Header from '@/components/Header'
-import Hero from '@/components/Hero'
+import Carousel from '@/components/Carousel'
 import Movies from '@/components/Movies'
 
 export default {
   components: {
     Header,
-    Hero,
+    Carousel,
     Movies,
   },
   data() {
     return {
       info: null,
       loading: true,
+      video: null,
     }
   },
   mounted() {
@@ -42,6 +58,12 @@ export default {
         console.log(error)
       })
       .finally(() => (this.loading = false))
+  },
+  computed: {
+    currentYear() {
+      const current = new Date().getFullYear()
+      return this.info.filter((year) => year.release_date.includes(current))
+    },
   },
 }
 </script>
