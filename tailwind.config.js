60e3fa19b2dfb21e832176b28ce5a7764422d1ac
Monique Silva <moniquesntsilva@gmail.com>
2022-01-22 23:04:16 -0300

    feat: add DM font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        DM: "'DM Sans', sans-serif",
+      },
+    },
   },
   variants: {
     extend: {},
