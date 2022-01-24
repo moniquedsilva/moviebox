eb87fbb3dbbb72b75170fdc855d24177e61d9d00
Monique Silva <moniquesntsilva@gmail.com>
2022-01-23 22:11:56 -0300

    feat: add footer

---
+++ src/components/Footer/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer
+    class="bg-black h-9 flex items-center text-center justify-center text-white"
+  >
+    <div>
+      Developed by
+      <a
+        href="https://github.com/moniquedsilva"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="underline"
+      >
+        Monique Silva</a
+      >.
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
