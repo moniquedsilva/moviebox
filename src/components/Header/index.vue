d7c83ca2c1aa8a854823b7ea3defaf1b61fb2868
Monique Silva <moniquesntsilva@gmail.com>
2022-01-23 22:12:19 -0300

    feat: add header

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,8 +1,15 @@
 <template>
-  <header class="absolute">
+  <header
+    class="bg-black md:bg-bg h-56 md:h-16 w-full md:absolute flex flex-col md:flex-row gap-x-8 items-center justify-center md:justify-around"
+  >
     <a href="/">
       <img src="assets/logo.svg" alt="Logo" />
     </a>
+    <nav class="flex flex-col md:flex-row gap-x-16 items-center text-white">
+      <a href="#featured" class="links"> <span> Featured Movie </span></a>
+      <a href="#new-arrival" class="links"> <span> New Arrival </span></a>
+      <a href="#tv-show" class="links"> <span> TV Shows </span></a>
+    </nav>
   </header>
 </template>
 
@@ -10,4 +17,39 @@
 export default {}
 </script>
 
-<style></style>
+<style scoped>
+.links {
+  position: relative;
+  width: max-content;
+  padding: 1rem 0.375rem;
+  font: 1rem;
+  font-weight: 900;
+  letter-spacing: 0.0625rem;
+  text-decoration: none;
+  text-transform: uppercase;
+}
+.links::after {
+  border-radius: 5px;
+  bottom: 0.875rem;
+  content: '';
+  display: block;
+  height: 0.5rem;
+  left: 0;
+  opacity: 0.3;
+  position: absolute;
+  right: 0;
+  transition: opacity 0.3s ease-in-out;
+  background-color: crimson;
+}
+.links:hover,
+.links:active {
+  color: white;
+}
+.links::after {
+  opacity: 1;
+}
+.links span {
+  position: relative;
+  z-index: 8;
+}
+</style>
