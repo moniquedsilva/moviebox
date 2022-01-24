5055816721ad144b1bed84b0fb44f458f9e80094
Monique Silva <moniquesntsilva@gmail.com>
2022-01-23 22:10:14 -0300

    feat: add bg color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      backgroundColor: {
+        bg: '#00000080',
+      },
       fontFamily: {
         DM: "'DM Sans', sans-serif",
       },
