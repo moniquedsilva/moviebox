41c2f6e99ac4d14b7efa931590124676e2266b6a
Monique Silva <moniquesntsilva@gmail.com>
2022-03-09 23:26:01 -0300

    feat: share link test

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -2,6 +2,9 @@
   <footer
     class="h-9 flex items-center text-center justify-center bg-black text-white"
   >
+    <button v-if="canShare()" @click="share">
+      <img src="/assets/icon_share.png" />
+    </button>
     <div>
       Developed by
       <a
@@ -15,3 +18,37 @@
     </div>
   </footer>
 </template>
+
+<script>
+export default {
+  name: "ShareLink",
+  methods: {
+    canShare() {
+      if (navigator.share) {
+        return true;
+      }
+      return false;
+    },
+    share() {
+      if (navigator.share) {
+        const title = "teste";
+        const text = "teste";
+        navigator
+          .share({
+            title,
+            url: window.location.href,
+            text,
+          })
+          .then(() => {
+            alert("Thanks for sharing!");
+          })
+          .catch((err) => {
+            alert(`Couldn't share because of ${err.message}`);
+          });
+      } else {
+        alert("Web Share API not supported.");
+      }
+    },
+  },
+};
+</script>
