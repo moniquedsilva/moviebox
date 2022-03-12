5d13cf9bc4b6ed2e84f99e68e2026d1bdd9e993f
Monique Silva <moniquesntsilva@gmail.com>
2022-03-12 13:55:22 -0300

    feat: add social share

---
+++ src/components/SocialShare/index.vue
@@ -0,0 +1,43 @@
+<template v-if="webShareAPI">
+  <button @click="webShare">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: "SocialShare",
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    url: {
+      type: String,
+      default: "",
+    },
+    text: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    webShareAPI() {
+      return navigator.share;
+    },
+  },
+  methods: {
+    webShare() {
+      if (navigator.share) {
+        navigator.share({
+          title: this.title,
+          text: this.text,
+          url: this.url,
+        });
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
